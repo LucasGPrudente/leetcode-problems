7d244ddcb01cf70418f14aed7b3f25654b262710
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-03-27 15:32:43 -0300

    Record the resolution of 13. Roman to Integer

---
+++ easy/13_roman_to_integer.java
@@ -0,0 +1,29 @@
+// Completed on 09/01/2024 (Last update on 24/02/2024)
+
+class Solution {
+    public int romanToInt(String s) {
+        int num = 0, val = 0, previousNum = 0;
+
+        for (int x = s.length() - 1; x >= 0; x--) {
+            switch (s.charAt(x)) {
+                case 'I' -> val = 1;
+                case 'V' -> val = 5;
+                case 'X' -> val = 10;
+                case 'L' -> val = 50;
+                case 'C' -> val = 100;
+                case 'D' -> val = 500;
+                case 'M' -> val = 1000;
+            }
+
+            if (val >= previousNum) {
+                num += val;
+            } else {
+                num -= val;
+            }
+
+            previousNum = val;
+        }
+
+        return num;
+    }
+}
