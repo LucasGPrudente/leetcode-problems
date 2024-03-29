8f0f7ebb7affbaea1e0f2d2b00c52c66d2bff676
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-03-29 14:42:47 -0300

    Update Solution.java

--- 0035_search_insert_position/Solution.java
+++ 0035_search_insert_position/Solution.java
@@ -1,1 +1,15 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int x = 0;
 
+        while (x < nums.length) {
+            if (target == nums[x] || target < nums[x]) {
+                return x;
+            }
+
+            x++;
+        }
+
+        return x;
+    }
+}
