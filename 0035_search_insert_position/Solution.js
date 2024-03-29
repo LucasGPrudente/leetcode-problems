59001ef971aad306addc8aed4f3803b1d30074da
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-03-29 14:42:29 -0300

    Update Solution.js

--- 0035_search_insert_position/Solution.js
+++ 0035_search_insert_position/Solution.js
@@ -1,1 +1,11 @@
+var searchInsert = function(nums, target) {
+   let i = 0;
 
+    for(i = 0; i < nums.length; i++){
+        if(target == nums[i] || target < nums[i]){
+            return i;
+        }
+    }
+
+   return i; 
+};
