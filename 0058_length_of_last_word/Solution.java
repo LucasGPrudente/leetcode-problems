7d3a554852175490211febd1aac03d1fa2314337
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-03-29 15:48:21 -0300

    Create Solution.java

---
+++ 0058_length_of_last_word/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int l = 0;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == ' ') break;
+
+            l++;
+        }
+
+        return l;
+    }
+}
