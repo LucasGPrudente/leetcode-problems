5915142c5f256bc66da29293a2d073d876be446e
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-03-29 15:55:19 -0300

    Create Solution.js

---
+++ 0058_length_of_last_word/Solution.js
@@ -0,0 +1,12 @@
+var lengthOfLastWord = function(s) {
+    let str = s.trim();
+    let l = 0;
+
+    for(let i = str.length - 1; i >= 0; i--){
+        if(str.charAt(i) == ' ') break;
+
+        l++;
+    }
+
+    return l;
+};
