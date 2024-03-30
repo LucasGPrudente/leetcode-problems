a0454f0ba4f2ef3399a7f2c9dbbead5a08098393
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-03-30 03:01:04 -0300

    Update Solution.java

--- 0070_climbing_stairs/Solution.java
+++ 0070_climbing_stairs/Solution.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
 
+        int ways = 0;
+        int[] sequence = { 1, 2 };
+
+        for (int x = 0; x < n - 2; x++) {
+            ways = sequence[0] + sequence[1];
+            sequence[0] = sequence[1];
+            sequence[1] = ways;
+        }
+        
+        return ways;
+    }
+}
