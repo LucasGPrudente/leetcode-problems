a8c43b5df0afb64da07b516e09d321e505e460ea
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-01 14:17:11 -0300

    Create Solution.java

---
+++ 3099_harshad_number/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int num = x;
+        int sum = 0;
+
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return (x % sum == 0) ? sum : -1;
+    }
+}
