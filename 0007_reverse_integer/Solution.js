c2813e44c95280bcb1dfea02a6ceb170993a3cf6
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-01 16:38:53 -0300

    Create Solution.js

---
+++ 0007_reverse_integer/Solution.js
@@ -0,0 +1,18 @@
+var reverse = function (x) {
+    let num = x;
+    let reversedNum = 0;
+
+    if(x < 0) num = num * -1;
+
+    while (num > 0) {
+        reversedNum = parseInt((reversedNum * 10) + (num % 10));
+        num = parseInt(num / 10);
+    }
+
+    if (reversedNum >= Math.pow(-2, 31) && reversedNum <= Math.pow(2, 31) - 1) {
+        if(x < 0) return reversedNum * -1;
+        return reversedNum;
+    }
+
+    return 0;
+};
