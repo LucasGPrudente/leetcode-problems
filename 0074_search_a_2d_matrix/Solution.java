71e1293f01015d67eb17ab555884c9355880b4d4
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-01 01:56:43 -0300

    Create Solution.java

---
+++ 0074_search_a_2d_matrix/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int columns = matrix.length;
+        int rows = matrix[0].length;
+
+        for(int i = 0; i < columns; i++){
+            for(int j = 0; j < rows; j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
