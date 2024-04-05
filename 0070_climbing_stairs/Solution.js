1a37971a8f93ff7a8b543d37a5ae074b96b88529
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-05 14:46:02 -0300

    Create Solution.js

---
+++ 0070_climbing_stairs/Solution.js
@@ -0,0 +1,13 @@
+var climbStairs = function (n) {
+    if (n <= 2) return n;
+
+    let ways = 0;
+    let sequence = [1, 2];
+
+    for (let i = 0; i < n - 2; i++) {
+        ways = sequence[0] + sequence[1];
+        sequence = [sequence[1], ways];
+    }
+
+    return ways;
+};
