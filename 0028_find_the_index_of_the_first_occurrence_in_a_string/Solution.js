e7c9789aefd7e4944fa45210a4f58075e9f66406
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-05 14:37:22 -0300

    Create Solution.js

---
+++ 0028_find_the_index_of_the_first_occurrence_in_a_string/Solution.js
@@ -0,0 +1,7 @@
+var strStr = function (haystack, needle) {
+    if (haystack.includes(needle)) return haystack.indexOf(needle);
+
+    return -1;
+};
+
+// return (haystack.includes(needle)) ? haystack.indexOf(needle) : -1;
