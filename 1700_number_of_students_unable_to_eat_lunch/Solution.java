e626d8604b497efcf7017fc57fdcbf27e254902a
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-07 23:16:19 -0300

    Create Solution.java

---
+++ 1700_number_of_students_unable_to_eat_lunch/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        List<Integer> stud = new ArrayList<>();
+        List<Integer> sand = new ArrayList<>();
+
+        for (int s : students) {
+            stud.add(s);
+        }
+        
+        for (int s : sandwiches) {
+            sand.add(s);
+        }
+
+        while (sand.size() > 0) {
+            if (!stud.contains(sand.get(0))) break;
+
+            stud.remove(stud.indexOf(sand.get(0)));
+            sand.remove(0);
+        }
+
+        return stud.size();
+    }
+}
