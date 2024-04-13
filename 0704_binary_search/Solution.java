2038254c2526b6be23485997f63dd1af04567f84
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-13 16:28:36 -0300

    Create Solution.java

---
+++ 0704_binary_search/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s = 0, m = 0, e = nums.length - 1;
+
+        while (s <= e) {
+            m = (s + e) / 2;
+
+            if (nums[m] == target) return m;
+            else if (nums[m] > target) e = m - 1;
+            else s = m + 1;
+        }
+
+        return -1;
+    }
+}
