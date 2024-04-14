1f073068c1ad0da168b64edbbc43553b6bf54849
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-14 18:36:02 -0300

    Create Solution.js

---
+++ 1137-n-th-tribonacci-number/Solution.js
@@ -0,0 +1,17 @@
+var tribonacci = function (n) {
+    let tribonacci = [0, 1, 1];
+    let nextNum = 0;
+
+    if (n <= 2) return tribonacci[n];
+
+    let i = 3;
+    while (i <= n) {
+        nextNum = tribonacci[0] + tribonacci[1] + tribonacci[2];
+        tribonacci[0] = tribonacci[1];
+        tribonacci[1] = tribonacci[2];
+        tribonacci[2] = nextNum;
+        i++;
+    }
+
+    return nextNum;
+};
