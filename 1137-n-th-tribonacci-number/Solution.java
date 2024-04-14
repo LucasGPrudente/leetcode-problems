2174d8cce709012981aa65f27f885b563f0b6451
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-14 18:36:40 -0300

    Create Solution.java

---
+++ 1137-n-th-tribonacci-number/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] sequence = new int[] { 0, 1, 1 };
+
+        if (n <= 2) return sequence[n];
+
+        for (int i = 3; i <= n; i++) {
+            int nextNum = sequence[0] + sequence[1] + sequence[2];
+            sequence[0] = sequence[1];
+            sequence[1] = sequence[2];
+            sequence[2] = nextNum;
+        }
+
+        return sequence[2];
+    }
+}
