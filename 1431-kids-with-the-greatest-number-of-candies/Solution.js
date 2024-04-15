dd6ef1889b942d7b47b5756c78f84d66d91979bc
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-15 02:24:38 -0300

    Create Solution.js

---
+++ 1431-kids-with-the-greatest-number-of-candies/Solution.js
@@ -0,0 +1,15 @@
+var kidsWithCandies = function (candies, extraCandies) {
+    let results = [];
+    let grtNum = 0;
+
+    for (let x of candies) {
+        if (x > grtNum) { grtNum = x; }
+    }
+
+    for (let x of candies) {
+        if (x + extraCandies >= grtNum) { results.push(true); }
+        else { results.push(false); }
+    }
+
+    return results;
+};
