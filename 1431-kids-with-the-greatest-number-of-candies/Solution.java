5778e0822ba3349c65d1a44d07e12f18101cf958
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-15 02:24:57 -0300

    Create Solution.java

---
+++ 1431-kids-with-the-greatest-number-of-candies/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> results = new ArrayList<>();
+        int grtNum = 0;
+
+        for (int x : candies) {
+            if (x > grtNum) { grtNum = x; };
+        }
+
+        for (int x : candies) {
+            if (x + extraCandies >= grtNum) { results.add(true); } 
+            else { results.add(false); }
+        }
+        
+        return results;
+    }
+}
