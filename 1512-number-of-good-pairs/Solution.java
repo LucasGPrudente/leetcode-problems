74a7ba6bc24eadc50db902c52cf92e405a0d5bb5
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-15 03:05:50 -0300

    Create Solution.java

---
+++ 1512-number-of-good-pairs/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int goodPairs = 0;
+
+        for (int x = 0; x < nums.length; x++) {
+            for (int z = x + 1; z < nums.length; z++) {
+                if (nums[x] == nums[z] && x < z) { goodPairs++; }
+            }
+        }
+
+        return goodPairs;
+    }
+}
