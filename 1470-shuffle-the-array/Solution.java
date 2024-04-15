61ca47896c9a7072b3e33b9ed9f8737f37e109d8
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-15 03:01:34 -0300

    Create Solution.java

---
+++ 1470-shuffle-the-array/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] newArr = new int[n * 2];
+
+        int i = 0;
+        int j = 0;
+        while (i < n && j < n * 2) {
+            newArr[j] = nums[i];
+            newArr[j + 1] = nums[i + n];
+
+            i += 1;
+            j += 2;
+        }
+
+        return newArr;
+    }
+}
