d26c39696a93a037a4120e77c4cb1ef90b071be2
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-17 17:37:27 -0300

    Update and rename Solution.js to Solution.js

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/Solution.js
@@ -0,0 +1,7 @@
+var strStr = function (haystack, needle) {
+    if (haystack.includes(needle)) {
+        return haystack.indexOf(needle);
+    }
+
+    return -1;
+};
--- 0028_find_the_index_of_the_first_occurrence_in_a_string/Solution.js
+++
@@ -1,7 +0,0 @@
-var strStr = function (haystack, needle) {
-    if (haystack.includes(needle)) return haystack.indexOf(needle);
-
-    return -1;
-};
-
-// return (haystack.includes(needle)) ? haystack.indexOf(needle) : -1;
