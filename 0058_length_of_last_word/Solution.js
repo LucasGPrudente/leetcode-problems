70bff5488d19421fa55d229f0595a6d6150dfad4
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-17 19:07:22 -0300

    Update Solution.js

--- 0058_length_of_last_word/Solution.js
+++ 0058_length_of_last_word/Solution.js
@@ -1,9 +1,11 @@
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
     let str = s.trim();
     let l = 0;
 
-    for(let i = str.length - 1; i >= 0; i--){
-        if(str.charAt(i) == ' ') break;
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str.charAt(i) == ' ') {
+            break;
+        }
 
         l++;
     }
