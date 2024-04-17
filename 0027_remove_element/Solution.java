098bc68266df3b24eb1efa463d50ac9c7870a70c
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-17 17:32:20 -0300

    Update Solution.java

--- 0027_remove_element/Solution.java
+++ 0027_remove_element/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int l = 0;
+        int j = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[l] = nums[i];
-                l++;
+                nums[j] = nums[i];
+                j++;
             }
         }
 
-        return l;
+        return j;
     }
 }
