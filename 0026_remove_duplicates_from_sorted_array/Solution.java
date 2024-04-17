62ac62b416b317552b212461e91a3ea28c43c353
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-17 17:07:39 -0300

    Update Solution.java

--- 0026_remove_duplicates_from_sorted_array/Solution.java
+++ 0026_remove_duplicates_from_sorted_array/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int lastUniqueNum = nums[0], z = 1;
+        int lastUniqueNum = nums[0];
+        int j = 1;
 
-        for (int x : nums) {
-            if (x != lastUniqueNum) {
-                nums[z] = x;
-                lastUniqueNum = nums[z];
-                z++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != lastUniqueNum) {
+                nums[j] = nums[i];
+                lastUniqueNum = nums[j];
+                j++;
             }
         }
 
-        return z;
+        return j;
     }
 }
