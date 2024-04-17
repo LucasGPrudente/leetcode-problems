16763601b843d1501235143d4f17d2ef1ebdb40d
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-17 04:31:22 -0300

    Create Solution.js

---
+++ 3115-maximum-prime-difference/Solution.js
@@ -0,0 +1,37 @@
+var maximumPrimeDifference = function (nums) {
+    let diff = 0;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (isPrime(nums[i])) {
+            diff = i;
+            break;
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (isPrime(nums[i])) {
+            diff -= i;
+            break;
+        }
+    }
+
+    return diff;
+};
+
+function isPrime(n) {
+    if (n == 2) {
+        return true;
+    }
+
+    if (n <= 1 || n % 2 == 0) {
+        return false;
+    }
+
+    for (let i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
