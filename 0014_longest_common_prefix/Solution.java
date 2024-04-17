fbc9d024ab943898ad467d119c92e58e525eeda2
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-17 16:45:31 -0300

    Update Solution.java

--- 0014_longest_common_prefix/Solution.java
+++ 0014_longest_common_prefix/Solution.java
@@ -2,22 +2,26 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         int minLength = strs[0].length();
 
-        for(String str : strs){
-            minLength = (str.length() < minLength) ? str.length() : minLength;
+        for (String str : strs) {
+            if (str.length() < minLength) {
+                minLength = str.length();
+            }
         }
 
         StringBuilder prefix = new StringBuilder("");
 
         outerloop: 
-        for (int i = 0; i < minLength; i++) {
-            char letter = strs[0].charAt(i);
+            for (int i = 0; i < minLength; i++) {
+                char letter = strs[0].charAt(i);
 
-            for (int j = 0; j < strs.length; j++) {
-                if (strs[j].charAt(i) != letter) break outerloop;
+                for (int j = 0; j < strs.length; j++) {
+                    if (strs[j].charAt(i) != letter) {
+                        break outerloop;
+                    }
+                }
+
+                prefix.append(letter);
             }
-
-            prefix.append(letter);
-        }
 
         return prefix.toString();
     }
