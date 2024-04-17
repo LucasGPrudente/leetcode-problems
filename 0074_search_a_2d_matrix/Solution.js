6561e89f5be7c599b6d095acbf218dc6f8f7698c
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-17 19:18:38 -0300

    Update Solution.js

--- 0074_search_a_2d_matrix/Solution.js
+++ 0074_search_a_2d_matrix/Solution.js
@@ -1,10 +1,12 @@
-var searchMatrix = function(matrix, target) {
+var searchMatrix = function (matrix, target) {
     let columns = matrix.length;
     let rows = matrix[0].length;
 
-    for(let i = 0; i < columns; i++){
-        for(let j = 0; j < rows; j++){
-            if(matrix[i][j] == target) return true;
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
         }
     }
 
