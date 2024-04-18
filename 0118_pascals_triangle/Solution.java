55e5420dc49a5111845fe676a8e14f813e97b6c3
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-18 06:36:16 -0300

    Update Solution.java

--- 0118_pascals_triangle/Solution.java
+++ 0118_pascals_triangle/Solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<List<Integer>>();
-        List<Integer> last = new ArrayList<>();
+        List<Integer> lastRow = new ArrayList<>();
 
         for (int x = 0; x < numRows; x++) {
-            List<Integer> temp = new ArrayList<>();
+            List<Integer> currRow = new ArrayList<>();
 
             for (int z = 0; z <= x; z++) {
                 if (z == 0 || z == x) {
-                    temp.add(1);
+                    currRow.add(1);
                     continue;
                 }
 
-                temp.add(last.get(z) + last.get(z - 1));
+                currRow.add(lastRow.get(z) + lastRow.get(z - 1));
             }
 
-            last = new ArrayList<>(temp);
-            triangle.add(temp);
+            lastRow = currRow;
+            triangle.add(currRow);
         }
 
         return triangle;
