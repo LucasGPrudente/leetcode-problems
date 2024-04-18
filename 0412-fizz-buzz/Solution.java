472a854a46fd7af0fd2e7967ba51bfce406ed525
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-18 18:27:09 -0300

    Update and rename Solution.java to Solution.java

---
+++ 0412-fizz-buzz/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+
+        for (int x = 1; x <= n; x++) {
+            if (x % 3 == 0 && x % 5 == 0) {
+                answer.add("FizzBuzz");
+            } else if (x % 5 == 0) {
+                answer.add("Buzz");
+            } else if (x % 3 == 0) {
+                answer.add("Fizz");
+            } else {
+                answer.add(String.valueOf(x));
+            }
+        }
+
+        return answer;
+    }
+}
