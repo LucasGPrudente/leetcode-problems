c9a692b3443335e9fc8cd59e4f4acf523bfb5cd8
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-19 09:26:43 -0300

    Create Solution.js

---
+++ 3079-find-the-sum-of-encrypted-integers/Solution.js
@@ -0,0 +1,26 @@
+var sumOfEncryptedInt = function (nums) {
+    let sum = 0;
+
+    for (let n of nums) {
+        let lgth = 0;
+        let larg = 0;
+
+        while (n > 0) {
+            let dig = n % 10;
+
+            if (dig > larg) {
+                larg = dig;
+            }
+
+            n = parseInt(n / 10);
+            lgth++;
+        }
+
+        // pl represents place value
+        for (let i = 0, pl = 1; i < lgth; i++, pl *= 10) {
+            sum += larg * pl;
+        }
+    }
+
+    return sum;
+};
