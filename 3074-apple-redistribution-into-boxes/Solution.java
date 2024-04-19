8726277be37526aa9d385ff8250e55c500fc57e7
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-19 09:24:50 -0300

    Create Solution.java

---
+++ 3074-apple-redistribution-into-boxes/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        Arrays.sort(capacity);
+
+        int minBoxes = 0;
+        int numApple = 0;
+
+        for (int x : apple) {
+            numApple += x;
+        }
+
+        for (int i = capacity.length - 1; i >= 0 && numApple > 0; i--) {
+            numApple -= capacity[i];
+            minBoxes += 1;
+        }
+
+        return minBoxes;
+    }
+}
