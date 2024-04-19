ee7157ac42f4b8c0326bbc1b8209020be4f6d185
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-19 09:26:24 -0300

    Create Solution.java

---
+++ 3079-find-the-sum-of-encrypted-integers/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int sumOfEncryptedInt(int[] nums) {
+        int sum = 0;
+
+        for (int n : nums) {
+            int lgth = 0;
+            int larg = 0;
+
+            while (n > 0) {
+                int dig = n % 10;
+
+                if (dig > larg) {
+                    larg = dig;
+                }
+
+                n /= 10;
+                lgth++;
+            }
+
+            // pl represents place value
+            for (int i = 0, pl = 1; i < lgth; i++, pl *= 10) {
+                sum += larg * pl;
+            }
+        }
+
+        return sum;
+    }
+}
