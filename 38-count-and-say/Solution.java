ffa42fe47f1d986dd122dbcd1b79b377b1e7f834
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-21 18:54:30 -0300

    Create Solution.java

---
+++ 38-count-and-say/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String countAndSay(int n) {
+        String sequence = "1";
+
+        for (int i = 1; i < n; i++) {
+            sequence = count(sequence);
+        }
+
+        return sequence;
+    }
+
+    public String count(String currStr) {
+        StringBuilder sequence = new StringBuilder("");
+        char[] arr = currStr.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i] - '0';
+            int times = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] != arr[i]) {
+                    break;
+                }
+
+                i = j;
+                times++;
+            }
+
+            sequence.append(times).append(num);
+        }
+
+        return sequence.toString();
+    }
+}
