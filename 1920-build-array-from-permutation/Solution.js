e3763be197963bc614b836396e452ee7bfa5960c
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-22 19:53:05 -0300

    Create Solution.js

---
+++ 1920-build-array-from-permutation/Solution.js
@@ -0,0 +1,16 @@
+// Build an array "ans" where ans[i] = nums[nums[i]]
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var buildArray = function (nums) {
+    let ans = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = nums[nums[i]];
+    }
+
+    return ans;
+};
