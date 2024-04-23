635e9d14d3aedede957cb3c0bfed261a2dd4955a
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-23 10:03:35 -0300

    Create Solution.java

---
+++ 1720-decode-xored-array/Solution.java
@@ -0,0 +1,15 @@
+// Decode the XORed array
+
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int[] decoded = new int[encoded.length + 1];
+
+        decoded[0] = first;
+
+        for (int i = 0; i < encoded.length; i++) {
+            decoded[i + 1] = decoded[i] ^ encoded[i];
+        }
+
+        return decoded;
+    }
+}
