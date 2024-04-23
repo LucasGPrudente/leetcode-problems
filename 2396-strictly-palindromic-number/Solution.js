1b3068d26869bf3561a70889f90af3fb62a350ca
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-23 10:36:41 -0300

    Create Solution.js

---
+++ 2396-strictly-palindromic-number/Solution.js
@@ -0,0 +1,19 @@
+// Return true if n is strictly palindromic and false otherwise
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var isStrictlyPalindromic = function (n) {
+    for (let b = 2; b <= n; b++) {
+        let orig = n.toString(b);
+        let rev = orig.split("").reverse().join("");
+
+        if (orig != rev) {
+            return false;
+        }
+    }
+
+    return true;
+};
