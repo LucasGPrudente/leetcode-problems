f11390a42ddc1f6dd1582a81d7d7f6df0f5f9132
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-29 08:40:18 -0300

    Update Solution.java

--- 2073-time-needed-to-buy-tickets/Solution.java
+++ 2073-time-needed-to-buy-tickets/Solution.java
@@ -1,14 +1,14 @@
+// Return the time taken for the person at position k to finish buying tickets
+
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         return calcTime(tickets, k, 0);
     }
 
-    public int calcTime(int[] tickets, int k, int time){
-        if(tickets[k] == 0) return time;
-
+    public int calcTime(int[] tickets, int k, int time) {
         for (int i = 0; i < tickets.length; i++) {
-            if (tickets[k] == 0) break;
-            if (tickets[i] == 0) continue;
+            if (tickets[k] == 0) { return time; }
+            if (tickets[i] == 0) { continue; }
 
             time++;
             tickets[i]--;
