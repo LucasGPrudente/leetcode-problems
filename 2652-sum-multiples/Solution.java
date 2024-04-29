8da6f7b126cdeca29c1a1b064ff050c1cb40cbd6
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-29 10:59:18 -0300

    Create Solution.java

---
+++ 2652-sum-multiples/Solution.java
@@ -0,0 +1,13 @@
+// Return the sum of all numbers divisible by 3, 5 or 7 in the range [1, n]
+
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) { sum += i; }
+        }
+
+        return sum;
+    }
+}
