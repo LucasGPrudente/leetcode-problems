863c01085f28bdb83d3f41a54c7448dea7087e54
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-29 08:40:05 -0300

    Update Solution.js

--- 2073-time-needed-to-buy-tickets/Solution.js
+++ 2073-time-needed-to-buy-tickets/Solution.js
@@ -1,7 +1,15 @@
+// Return the time taken for the person at position k to finish buying tickets
+
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+
 var timeRequiredToBuy = function (tickets, k, time = 0) {
     for (let i = 0; i < tickets.length; i++) {
-        if (tickets[k] == 0) return time;
-        if (tickets[i] == 0) continue;
+        if (tickets[k] == 0) { return time; }
+        if (tickets[i] == 0) { continue; }
 
         time++;
         tickets[i]--;
