02cbffe1e86416a5578f5895d99a82d4c4b1556f
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-29 16:35:16 -0300

    Create Solution.java

---
+++ 2942-find-words-containing-character/Solution.java
@@ -0,0 +1,13 @@
+// Return an array of indices representing the words that contain the character x
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains(String.valueOf(x))) { ans.add(i); }
+        }
+
+        return ans;
+    }
+}
