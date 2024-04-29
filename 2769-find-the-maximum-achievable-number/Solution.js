13c4eac7f63adcadcf9af677da39ae6dc96501b7
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-29 16:29:49 -0300

    Create Solution.js

---
+++ 2769-find-the-maximum-achievable-number/Solution.js
@@ -0,0 +1,11 @@
+// Return the maximum possible achievable number
+
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+
+var theMaximumAchievableX = function (num, t) {
+    return num + (t * 2);
+};
