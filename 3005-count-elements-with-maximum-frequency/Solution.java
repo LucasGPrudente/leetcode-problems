6c269bfa934684b987f1c651511f3ff98971ec77
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-29 16:51:55 -0300

    Create Solution.java

---
+++ 3005-count-elements-with-maximum-frequency/Solution.java
@@ -0,0 +1,22 @@
+// Return the sum of the frequencies of the elements with the highest frequency in nums
+
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int[] elem = new int[101];
+
+        int maxFreq = 0;
+        int occurr = 0;
+
+        for (int n : nums) {
+            elem[n]++;
+
+            if (elem[n] > maxFreq) { maxFreq = elem[n]; }
+        }
+
+        for (int freq : elem) {
+            if (freq == maxFreq) { occurr++; }
+        }
+
+        return maxFreq * occurr;
+    }
+}
