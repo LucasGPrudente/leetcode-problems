c0b8a280397264b87db70ce33b9ee05e331321c6
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:48:03 -0300

    Update Solution.js

--- 0704-binary-search/Solution.js
+++ 0704-binary-search/Solution.js
@@ -1,12 +1,21 @@
-var search = function (nums, target) {
-    let s = 0, m = 0, e = nums.length;
+// Write a function to search target in nums
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function (nums, target) {
+    let s = 0;
+    let m = 0;
+    let e = nums.length;
     while (s <= e) {
         m = parseInt((e + s) / 2);
 
-        if (nums[m] == target) return m;
-        else if (nums[m] < target) s = m + 1;
-        else e = m - 1;
+        if (nums[m] == target) { return m; }
+        else if (nums[m] < target) { s = m + 1; }
+        else { e = m - 1; }
     }
 
     return -1;
