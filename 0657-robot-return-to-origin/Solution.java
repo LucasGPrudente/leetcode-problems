237d530ad9df57017890e2d1fe787b2ef69fc915
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:41:28 -0300

    Update Solution.java

--- 0657-robot-return-to-origin/Solution.java
+++ 0657-robot-return-to-origin/Solution.java
@@ -1,12 +1,17 @@
+// Return true if the robot returns to the origin after it finishes all of its moves
+
 class Solution {
     public boolean judgeCircle(String moves) {
-        int r = 0, l = 0, u = 0, d = 0;
+        int r = 0;
+        int l = 0;
+        int u = 0;
+        int d = 0;
 
         for (char m : moves.toCharArray()) {
-            if (m == 'R') r++;
-            else if (m == 'L') l++;
-            else if (m == 'U') u++;
-            else d++;
+            if (m == 'R') { r++; }
+            else if (m == 'L') { l++; }
+            else if (m == 'U') { u++; }
+            else { d++; }
         }
 
         return (r == l) && (u == d);
