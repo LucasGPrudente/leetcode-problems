f951f5eb6ae0a167012a511244d2d13d42718f6c
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:42:44 -0300

    Update Solution.js

--- 0657-robot-return-to-origin/Solution.js
+++ 0657-robot-return-to-origin/Solution.js
@@ -1,13 +1,21 @@
+// Return true if the robot returns to the origin after it finishes all of its moves
+
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+
 var judgeCircle = function (moves) {
     let arr = moves.split("");
-    let horiz = 0, vert = 0;
+    let horiz = 0;
+    let vert = 0;
 
     arr.forEach((e) => {
-        if (e == 'L') horiz++;
-        else if (e == 'R') horiz--;
-        else if (e == 'U') vert++;
-        else vert--;
+        if (e == 'L') { horiz++; }
+        else if (e == 'R') { horiz--; }
+        else if (e == 'U') { vert++; }
+        else { vert--; }
     });
 
-    return (horiz == 0) && (vert == 0);
+    return horiz == 0 && vert == 0;
 };
