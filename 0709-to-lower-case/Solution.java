541d57e7264718ec6335391e32e15a0a4eb7d10d
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:50:52 -0300

    Update Solution.java

--- 0709-to-lower-case/Solution.java
+++ 0709-to-lower-case/Solution.java
@@ -1,9 +1,11 @@
+// Return the string after replacing every uppercase letter with the same lowercase letter
+
 class Solution {
     public String toLowerCase(String s) {
         StringBuilder ans = new StringBuilder();
 
-        for(char c : s.toCharArray()){
-            if(c >= 'A' && c <= 'Z') c = (char) ((int) c + 32);
+        for (char c : s.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') { c = (char) ((int) c + 32); }
             ans.append(c);
         }
 
