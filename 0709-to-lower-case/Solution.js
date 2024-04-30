9db32c3566fb0a03cb996108fa47fa1c7753fb7d
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:50:12 -0300

    Update Solution.js

--- 0709-to-lower-case/Solution.js
+++ 0709-to-lower-case/Solution.js
@@ -1,10 +1,17 @@
+// Return the string after replacing every uppercase letter with the same lowercase letter
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
 var toLowerCase = function (s) {
     let lower = "";
 
     s.split("").forEach((e) => {
         let code = e.charCodeAt(0);
 
-        if (code >= 65 && code <= 90) e = String.fromCharCode(code + 32);
+        if (code >= 65 && code <= 90) { e = String.fromCharCode(code + 32); }
 
         lower += e;
     });
