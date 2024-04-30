6e43b26d6b4df1821bc48912277a152dc8db9eff
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:53:24 -0300

    Update Solution.js

--- 0771-jewels-and-stones/Solution.js
+++ 0771-jewels-and-stones/Solution.js
@@ -1,8 +1,16 @@
+// Return how many of the stones are also jewels
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+
 var numJewelsInStones = function (jewels, stones) {
     let count = 0;
 
     for (let x of stones) {
-        if (jewels.indexOf(x) > -1) count++;
+        if (jewels.indexOf(x) > -1) { count++; }
     }
 
     return count;
