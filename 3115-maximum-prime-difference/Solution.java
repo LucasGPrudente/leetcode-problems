4fd8b004a8f2454432302a2761417a700cd24750
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 09:03:12 -0300

    Update Solution.java

--- 3115-maximum-prime-difference/Solution.java
+++ 3115-maximum-prime-difference/Solution.java
@@ -1,3 +1,5 @@
+// Return the maximum distance between the indices of two prime numbers in nums
+
 class Solution {
     public int maximumPrimeDifference(int[] nums) {
         int diff = 0;
@@ -20,18 +22,12 @@ class Solution {
     }
 
     public boolean isPrime(int n) {
-        if (n == 2) {
-            return true;
-        }
+        if (n == 2) { return true; }
 
-        if (n <= 1 || n % 2 == 0) {
-            return false;
-        }
+        if (n <= 1 || n % 2 == 0) { return false; }
 
         for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
-                return false;
-            }
+            if (n % i == 0) { return false; }
         }
 
         return true;
