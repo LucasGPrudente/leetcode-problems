8c73e00d4950eef11ac698a15a8f369f31c51a65
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:53:49 -0300

    Update Solution.java

--- 0771-jewels-and-stones/Solution.java
+++ 0771-jewels-and-stones/Solution.java
@@ -1,9 +1,11 @@
+// Return how many of the stones are also jewels
+
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int numOfJewels = 0;
 
         for (char c : stones.toCharArray()) {
-            if (jewels.indexOf(c) > -1) numOfJewels++;
+            if (jewels.indexOf(c) > -1) { numOfJewels++; }
         }
 
         return numOfJewels;
