6e11c61f21375ce5e0fada059637b79bd671c02d
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 06:53:35 -0300

    Update Solution.js

--- 0495-teemo-attacking/Solution.js
+++ 0495-teemo-attacking/Solution.js
@@ -1,13 +1,19 @@
+// Return the total number of seconds that Ashe is poisoned
+
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+
 var findPoisonedDuration = function (timeSeries, duration) {
     let poisoned = 0;
 
-    timeSeries.forEach((e, i) => {
-        if(i == timeSeries.length - 1) return;
-
-        let interval = timeSeries[i + 1] - e;
+    for (let i = 0; i < timeSeries.length - 1; i++) {
+        let interval = parseInt(timeSeries[i + 1] - timeSeries[i]);
 
         poisoned += (interval >= duration) ? duration : interval;
-    });
+    }
 
     return poisoned + duration;
 };
