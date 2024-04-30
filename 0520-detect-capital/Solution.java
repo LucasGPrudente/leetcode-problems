787d909bb1d87560070f1c69d3ce1ae7c1ea9380
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 07:03:12 -0300

    Update Solution.java

--- 0520-detect-capital/Solution.java
+++ 0520-detect-capital/Solution.java
@@ -1,15 +1,16 @@
+// Return true if the usage of capitals in it is right
+
 class Solution {
     public boolean detectCapitalUse(String word) {
         boolean firstLetterIsCap = Character.isUpperCase(word.charAt(0));
         int cap = 0;
 
         for (int i = 0; i < word.length(); i++) {
-            if (Character.isLowerCase(word.charAt(i))) continue;
+            if (Character.isLowerCase(word.charAt(i))) { continue; }
+
             cap++;
         }
 
-        boolean isValid = cap == 0 || cap == word.length() || (cap == 1 && firstLetterIsCap);
-
-        return isValid;
+        return cap == 0 || cap == word.length() || (cap == 1 && firstLetterIsCap);
     }
 }
