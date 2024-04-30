887883594b5a998d2c7d9f2925996240ec335af7
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:58:28 -0300

    Update Solution.java

--- 0896-monotonic-array/Solution.java
+++ 0896-monotonic-array/Solution.java
@@ -1,12 +1,14 @@
+// Return true if the given array is monotonic
+
 class Solution {
     public boolean isMonotonic(int[] nums) {
         if (nums[0] <= nums[nums.length - 1]) {
             for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] > nums[i + 1]) return false;
+                if (nums[i] > nums[i + 1]) { return false; }
             }
         } else {
             for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] < nums[i + 1]) return false;
+                if (nums[i] < nums[i + 1]) { return false; }
             }
         }
 
