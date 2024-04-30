737ca4fe89c3f904cef011b6aea14e7f320fefe9
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-04-30 08:59:16 -0300

    Update Solution.js

--- 0896-monotonic-array/Solution.js
+++ 0896-monotonic-array/Solution.js
@@ -1,15 +1,22 @@
+// Return true if the given array is monotonic
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
 var isMonotonic = function (nums) {
     let length = nums.length;
     let lastNum = nums[0];
 
     if (nums[0] <= nums[length - 1]) {
         for (let i = 1; i < length; i++) {
-            if (lastNum > nums[i]) return false;
+            if (lastNum > nums[i]) { return false; }
             lastNum = nums[i];
         }
     } else {
         for (let i = 1; i < length; i++) {
-            if (lastNum < nums[i]) return false;
+            if (lastNum < nums[i]) { return false; }
             lastNum = nums[i];
         }
     }
