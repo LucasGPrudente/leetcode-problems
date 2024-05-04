250aa0d15053a1f0ae9a96d5410e56cfcfecf325
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-04 19:54:04 -0300

    Update Solution.java

--- algorithms/0013-roman-to-integer/Solution.java
+++ algorithms/0013-roman-to-integer/Solution.java
@@ -1,1 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+        int num = 0; 
+        int val = 0; 
+        int previousNum = 0;
 
+        for (int x = s.length() - 1; x >= 0; x--) {
+            switch (s.charAt(x)) {
+                case 'I' -> val = 1;
+                case 'V' -> val = 5;
+                case 'X' -> val = 10;
+                case 'L' -> val = 50;
+                case 'C' -> val = 100;
+                case 'D' -> val = 500;
+                case 'M' -> val = 1000;
+            }
+
+            if (val >= previousNum) {
+                num += val;
+            } else {
+                num -= val;
+            }
+
+            previousNum = val;
+        }
+
+        return num;
+    }
+}
