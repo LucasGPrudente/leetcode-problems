031a0794db26d044246d94179dfafacc9c77ac4b
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-04 19:50:09 -0300

    Create Solution.java

---
+++ algorithms/0007-reverse-integer/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        long reversedNum = 0;
+
+        while (x != 0) {
+            reversedNum = (reversedNum * 10) + (x % 10);
+            x /= 10;
+        }
+
+        if (reversedNum < Math.pow(-2, 31) || reversedNum > Math.pow(2, 31) - 1) {
+            return 0;
+        }
+        
+        return (int) reversedNum;
+    }
+}
