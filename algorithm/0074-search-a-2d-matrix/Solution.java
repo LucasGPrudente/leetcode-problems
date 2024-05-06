674bb3018dc1ed0191082ecfca45a67f06eb35ee
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-06 13:55:37 -0300

    Update Solution.java

--- algorithm/0074-search-a-2d-matrix/Solution.java
+++ algorithm/0074-search-a-2d-matrix/Solution.java
@@ -1,11 +1,13 @@
+// Return true if target is in matrix
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int columns = matrix.length;
         int rows = matrix[0].length;
 
-        for(int i = 0; i < columns; i++){
-            for(int j = 0; j < rows; j++){
-                if(matrix[i][j] == target){
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (matrix[i][j] == target) {
                     return true;
                 }
             }
