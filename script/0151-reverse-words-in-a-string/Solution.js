ddeab76091efe5a73c22944783548a02e5bf018f
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-06 14:02:09 -0300

    Update and rename 0151-reverse-words-in-a-string/Solution.js to script/0151-reverse-words-in-a-string/Solution.js

--- script/0151-reverse-words-in-a-string/Solution.js
+++ script/0151-reverse-words-in-a-string/Solution.js
@@ -1,3 +1,10 @@
+// Return a string of the words in reverse order concatenated by a single space
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
 var reverseWords = function (s) {
     const phase = s.replace(/  +/g, ' ').trim();
     const arr = phase.split(" ");
