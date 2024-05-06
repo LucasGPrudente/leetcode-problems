6ae6f4c1e86eb4dc86f4ba96a5c01e9e49ecf52f
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-06 13:57:10 -0300

    Update Solution.js

--- script/0074-search-a-2d-matrix/Solution.js
+++ script/0074-search-a-2d-matrix/Solution.js
@@ -1,12 +1,18 @@
+// Return true if target is in matrix
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+
 var searchMatrix = function (matrix, target) {
     let columns = matrix.length;
     let rows = matrix[0].length;
 
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
-            if (matrix[i][j] == target) {
-                return true;
-            }
+            if (matrix[i][j] == target) { return true; }
         }
     }
 
