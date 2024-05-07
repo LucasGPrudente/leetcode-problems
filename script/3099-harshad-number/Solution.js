d7821b3d8756564502018c49fc449d585fefbd6c
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 09:32:22 -0300

    Update Solution.js

--- script/3099-harshad-number/Solution.js
+++ script/3099-harshad-number/Solution.js
@@ -1,13 +1,20 @@
-var sumOfTheDigitsOfHarshadNumber = function(x) {
+// Return the sum of the digits of x
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+var sumOfTheDigitsOfHarshadNumber = function (x) {
     let num = x;
     let sum = 0;
 
-    while(num > 0){
+    while (num > 0) {
         sum = parseInt(sum + num % 10);
         num = parseInt(num / 10);
     }
 
-    if(x % sum == 0) return sum;
+    if (x % sum == 0) { return sum; }
 
-    return -1; 
+    return -1;
 };
