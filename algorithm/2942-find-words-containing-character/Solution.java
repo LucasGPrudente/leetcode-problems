d46bebb8d4688a3d50dcb515c6353b6150d2b239
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 10:43:36 -0300

    Update and rename 2942-find-words-containing-character/Solution.java to algorithm/2942-find-words-containing-character/Solution.java

--- algorithm/2942-find-words-containing-character/Solution.java
+++ algorithm/2942-find-words-containing-character/Solution.java
@@ -5,7 +5,9 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
 
         for (int i = 0; i < words.length; i++) {
-            if (words[i].contains(String.valueOf(x))) { ans.add(i); }
+            if (words[i].contains(String.valueOf(x))) {
+                ans.add(i);
+            }
         }
 
         return ans;
