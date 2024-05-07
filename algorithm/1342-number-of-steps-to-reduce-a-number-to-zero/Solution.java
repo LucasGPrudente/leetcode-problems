d40cc47a710104b234e72f175deeb6be53c22418
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 15:59:40 -0300

    Update and rename 1342-number-of-steps-to-reduce-a-number-to-zero/Solution.java to algorithm/1342-number-of-steps-to-reduce-a-number-to-zero/Solution.java

--- algorithm/1342-number-of-steps-to-reduce-a-number-to-zero/Solution.java
+++ algorithm/1342-number-of-steps-to-reduce-a-number-to-zero/Solution.java
@@ -1,11 +1,16 @@
+// Return the number of steps to reduce it to zero
+
 class Solution {
     public int numberOfSteps(int num) {
         int steps = 0;
 
         while (num > 0) {
-            if (num % 2 == 0) { num /= 2; }
-            else { num--; }
-            
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num--;
+            }
+
             steps++;
         }
 
