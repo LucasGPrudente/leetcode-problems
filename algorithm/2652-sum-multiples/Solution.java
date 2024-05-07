febac2b5f0fd5e68fdde69f293bf69f4a502490a
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 10:58:50 -0300

    Update and rename 2652-sum-multiples/Solution.java to algorithm/2652-sum-multiples/Solution.java

--- algorithm/2652-sum-multiples/Solution.java
+++ algorithm/2652-sum-multiples/Solution.java
@@ -5,7 +5,9 @@ class Solution {
         int sum = 0;
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) { sum += i; }
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+            }
         }
 
         return sum;
