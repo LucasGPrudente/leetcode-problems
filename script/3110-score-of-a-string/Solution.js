2b9dbf64d31d95280ec780cf8d9e24eee9882b23
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 09:36:29 -0300

    Update Solution.js

--- script/3110-score-of-a-string/Solution.js
+++ script/3110-score-of-a-string/Solution.js
@@ -1,9 +1,18 @@
+// Return the score of s
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
 var scoreOfString = function (s) {
     let score = 0;
 
     for (let i = 0; i < s.length - 1; i++) {
         let temp = s.charCodeAt(i) - s.charCodeAt(i + 1);
+
         if (temp < 0) { temp *= -1; }
+        
         score += temp;
     }
 
