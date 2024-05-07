fc2466699b75f7cca54c5504c96d3f571aa5db0a
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 15:39:39 -0300

    Update and rename 1470-shuffle-the-array/Solution.js to script/1470-shuffle-the-array/Solution.js

--- script/1470-shuffle-the-array/Solution.js
+++ script/1470-shuffle-the-array/Solution.js
@@ -1,3 +1,11 @@
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn]
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+
 var shuffle = function (nums, n) {
     let newArr = [];
 
