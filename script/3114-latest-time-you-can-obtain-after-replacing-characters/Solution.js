6f4a9f3edd24a3d3cf02a9d2530c9790720f2a6a
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 09:40:32 -0300

    Update Solution.js

--- script/3114-latest-time-you-can-obtain-after-replacing-characters/Solution.js
+++ script/3114-latest-time-you-can-obtain-after-replacing-characters/Solution.js
@@ -1,3 +1,10 @@
+// Replace all "?" characters in s with digits such that the time is a string with a 12-hour time and is as recent as possible
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
 var findLatestTime = function (s) {
     let ans = s.split("");
 
