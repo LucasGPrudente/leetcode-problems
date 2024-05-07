862ae8309cb82f36686101adc950706a6ac50a8c
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 09:35:47 -0300

    Update Solution.java

--- algorithm/3110-score-of-a-string/Solution.java
+++ algorithm/3110-score-of-a-string/Solution.java
@@ -1,11 +1,13 @@
+// Return the score of s
+
 class Solution {
     public int scoreOfString(String s) {
         int score = 0;
 
         for (int i = 0; i < s.length() - 1; i++) {
             int temp = s.charAt(i) - s.charAt(i + 1);
-            if (temp < 0) { temp *= -1; }
-            score += temp;
+
+            score += (temp > 0) ? temp : temp * -1;
         }
 
         return score;
