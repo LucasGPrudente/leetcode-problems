20486fe08ac7c21f16f2769a842429ed08a625ef
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 15:25:37 -0300

    Update and rename 1700-number-of-students-unable-to-eat-lunch/Solution.java to algorithm/1700-number-of-students-unable-to-eat-lunch/Solution.java

--- algorithm/1700-number-of-students-unable-to-eat-lunch/Solution.java
+++ algorithm/1700-number-of-students-unable-to-eat-lunch/Solution.java
@@ -1,3 +1,5 @@
+// Return the number of students that are unable to eat
+
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         List<Integer> stud = new ArrayList<>();
@@ -6,13 +8,15 @@ class Solution {
         for (int s : students) {
             stud.add(s);
         }
-        
+
         for (int s : sandwiches) {
             sand.add(s);
         }
 
         while (sand.size() > 0) {
-            if (!stud.contains(sand.get(0))) break;
+            if (!stud.contains(sand.get(0))) {
+                break;
+            }
 
             stud.remove(stud.indexOf(sand.get(0)));
             sand.remove(0);
