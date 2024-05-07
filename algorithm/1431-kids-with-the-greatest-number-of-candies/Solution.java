1a199dbc1aa7e924bef2a8b3fad1ffbdb495c43f
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-07 15:55:52 -0300

    Update and rename 1431-kids-with-the-greatest-number-of-candies/Solution.java to algorithm/1431-kids-with-the-greatest-number-of-candies/Solution.java

--- algorithm/1431-kids-with-the-greatest-number-of-candies/Solution.java
+++ algorithm/1431-kids-with-the-greatest-number-of-candies/Solution.java
@@ -1,17 +1,25 @@
+// Return a boolean array result
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> results = new ArrayList<>();
         int grtNum = 0;
 
         for (int x : candies) {
-            if (x > grtNum) { grtNum = x; };
+            if (x > grtNum) {
+                grtNum = x;
+            }
+            ;
         }
 
         for (int x : candies) {
-            if (x + extraCandies >= grtNum) { results.add(true); } 
-            else { results.add(false); }
+            if (x + extraCandies >= grtNum) {
+                results.add(true);
+            } else {
+                results.add(false);
+            }
         }
-        
+
         return results;
     }
 }
