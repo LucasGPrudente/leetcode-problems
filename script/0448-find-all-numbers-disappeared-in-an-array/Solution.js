390442f006e72bbee2d0b862ac11747255c74be8
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-08 09:06:29 -0300

    Update and rename 0448-find-all-numbers-disappeared-in-an-array/Solution.js to script/0448-find-all-numbers-disappeared-in-an-array/Solution.js

--- script/0448-find-all-numbers-disappeared-in-an-array/Solution.js
+++ script/0448-find-all-numbers-disappeared-in-an-array/Solution.js
@@ -1,3 +1,10 @@
+// Return an array of all the integers in the range [1, n] that do not appear in nums
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
 var findDisappearedNumbers = function (nums) {
     let ans = [];
     let temp = new Array(nums.length + 1).fill(0);
