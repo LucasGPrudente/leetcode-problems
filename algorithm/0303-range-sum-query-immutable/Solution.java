29b638b48f1279a30bf9e3f3871cc97c26a5f761
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-08 09:15:26 -0300

    Update and rename 0303-range-sum-query-immutable/Solution.java to algorithm/0303-range-sum-query-immutable/Solution.java

--- algorithm/0303-range-sum-query-immutable/Solution.java
+++ algorithm/0303-range-sum-query-immutable/Solution.java
@@ -1,14 +1,16 @@
+// Returns the sum of the elements of nums between indices left and right
+
 class NumArray {
     int[] arrSum;
 
     public NumArray(int[] nums) {
         arrSum = nums;
 
-        for(int i = 1; i < arrSum.length; i++){
+        for (int i = 1; i < arrSum.length; i++) {
             arrSum[i] = arrSum[i] + arrSum[i - 1];
         }
     }
-    
+
     public int sumRange(int l, int r) {
         return (l == 0) ? arrSum[r] : arrSum[r] - arrSum[l - 1];
     }
