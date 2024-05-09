859a2ed77c01b915f302517e7dfd50f7ed987844
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-09 09:03:28 -0300

    Update and rename 0027-remove-element/Solution.js to script/0027-remove-element/Solution.js

--- script/0027-remove-element/Solution.js
+++ script/0027-remove-element/Solution.js
@@ -1,3 +1,11 @@
+// Return the number of elements in nums which are not equal to val
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
 var removeElement = function(nums, val) {
     for(let i = 0; i < nums.length; i++){
         if(nums[i] === val){
