ec0abcc86c16b5b6ee5c90347a3ec77ab412990e
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-09 09:07:28 -0300

    Update and rename 0014-longest-common-prefix/Solution.java to algorithm/0014-longest-common-prefix/Solution.java

---
+++ algorithm/0014-longest-common-prefix/Solution.java
@@ -0,0 +1,30 @@
+// Find the longest common prefix string amongst an array of strings
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int minLength = strs[0].length();
+
+        for (String str : strs) {
+            if (str.length() < minLength) {
+                minLength = str.length();
+            }
+        }
+
+        StringBuilder prefix = new StringBuilder("");
+
+        outerloop: 
+            for (int i = 0; i < minLength; i++) {
+                char letter = strs[0].charAt(i);
+
+                for (int j = 0; j < strs.length; j++) {
+                    if (strs[j].charAt(i) != letter) {
+                        break outerloop;
+                    }
+                }
+
+                prefix.append(letter);
+            }
+
+        return prefix.toString();
+    }
+}
