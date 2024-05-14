22730c6ce52138d1a74625245b9aced00ff71087
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-14 11:30:33 -0300

    Create Solution.js

---
+++ script/3131-find-the-integer-added-to-array-i/Solution.js
@@ -0,0 +1,14 @@
+// Return the integer x
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+
+var addedInteger = function (nums1, nums2) {
+    nums1.sort((a, b) => { return a - b; });
+    nums2.sort((a, b) => { return a - b; });
+
+    return nums2[0] - nums1[0];
+};
