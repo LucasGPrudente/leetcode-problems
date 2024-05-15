b866621a4d1b78ed03c7c7a137a54e332ee34b6b
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-15 14:06:05 -0300

    Create Solution.java

---
+++ algorithm/3024-type-of-triangle/Solution.java
@@ -0,0 +1,15 @@
+// Return a string representing the type of triangle or "none"
+
+class Solution {
+    public String triangleType(int[] nums) {
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]) {
+            return "none";
+        } else if (nums[0] == nums[1] && nums[0] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
