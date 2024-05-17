5d3ca0077e8c2a9fed287b6b5ac482514f8f2763
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-17 18:55:11 -0300

    Update Solution.js

--- script/0200-number-of-islands/Solution.js
+++ script/0200-number-of-islands/Solution.js
@@ -8,23 +8,28 @@
 var numIslands = function (grid) {
     let isl = 0;
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
+    let m = grid.length;
+    let n = grid[0].length;
+
+    const mark = (grid, i, j) => {
+        if (grid[i][j] == 0 || grid[i][j] == '*') { return; }
+
+        grid[i][j] = '*';
+
+        if (i > 0) { mark(grid, i - 1, j); }
+        if (j > 0) { mark(grid, i, j - 1); }
+        if (i < m - 1) { mark(grid, i + 1, j); }
+        if (j < n - 1) { mark(grid, i, j + 1); }
+    };
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             if (grid[i][j] == '1') {
-                mark(grid, i, j);
                 isl++;
+                mark(grid, i, j);
             }
         }
     }
 
     return isl;
-
-    function mark(grid, i, j) {
-        grid[i][j] = '*';
-
-        if (i > 0 && grid[i - 1][j] == '1') { mark(grid, i - 1, j); }
-        if (j > 0 && grid[i][j - 1] == '1') { mark(grid, i, j - 1); }
-        if (i < grid.length - 1 && grid[i + 1][j] == '1') { mark(grid, i + 1, j); }
-        if (j < grid[0].length - 1 && grid[i][j + 1] == '1') { mark(grid, i, j + 1); }
-    }
 };
