f4071af55b2e51cca19791216a46ef60ba8473f6
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-24 00:17:04 -0300

    Create Solution.js

---
+++ script/3069-distribute-elements-into-two-arrays-i/Solution.js
@@ -0,0 +1,21 @@
+// After distribute all the elements of nums between two arrays arr1 and arr2, return the array result
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var resultArray = function (nums) {
+    let arr1 = [nums[0]];
+    let arr2 = [nums[1]];
+
+    for (let i = 2; i < nums.length; i++) {
+        if (arr1[arr1.length - 1] > arr2[arr2.length - 1]) {
+            arr1.push(nums[i]);
+        } else {
+            arr2.push(nums[i]);
+        }
+    }
+
+    return arr1.concat(arr2);
+};
