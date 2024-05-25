8afb8f703dfb0da4a292fb0d9179e21fa208baec
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-25 18:41:35 -0300

    Update Solution.java

--- algorithm/2785-sort-vowels-in-a-string/Solution.java
+++ algorithm/2785-sort-vowels-in-a-string/Solution.java
@@ -1,1 +1,30 @@
+// Sort the vowels in string, return the resulting string
 
+class Solution {
+    public String sortVowels(String str) {
+        List<Character> vows = new ArrayList<Character>();
+
+        int l = str.length();
+
+        char[] newStr = str.toCharArray();
+
+        for (int i = 0; i < l; i++) {
+            char c = str.charAt(i);
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                newStr[i] = '*';
+                vows.add(c);
+            }
+        }
+
+        Collections.sort(vows);
+
+        for (int i = 0, z = 0; i < l; i++) {
+            if (newStr[i] == '*') {
+                newStr[i] = vows.get(z++);
+            }
+        }
+
+        return String.valueOf(newStr);
+    }
+}
