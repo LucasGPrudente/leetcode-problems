cd6655e2c29b79a5ac904325e014e5831332b2d2
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-05-25 18:41:32 -0300

    Update Solution.js

--- script/2785-sort-vowels-in-a-string/Solution.js
+++ script/2785-sort-vowels-in-a-string/Solution.js
@@ -1,1 +1,35 @@
+// Sort the vowels in string, return the resulting string
 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var sortVowels = function (s) {
+    s = s.split("");
+
+    let vows = [];
+
+    s.forEach((c, i) => {
+        if (isVowel(c)) {
+            vows.push(c);
+            s[i] = "*";
+        }
+    });
+
+    vows.sort();
+
+    for (let i = 0, j = 0; i < s.length; i++) {
+        if (s[i] == "*") {
+            s[i] = vows[j++];
+        }
+    }
+
+    return s.join("");
+
+    function isVowel(c) {
+        c = c.toLowerCase();
+
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+};
