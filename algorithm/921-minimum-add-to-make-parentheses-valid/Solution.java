03e830987c9a41b28fdc0794606a678d80a29ec0
Lucas Prudente <165199182+LucasGPrudente@users.noreply.github.com>
2024-06-18 15:42:05 -0300

    Update Solution.java

--- algorithm/921-minimum-add-to-make-parentheses-valid/Solution.java
+++ algorithm/921-minimum-add-to-make-parentheses-valid/Solution.java
@@ -1,1 +1,22 @@
+// Return the minimum number of moves required to make s valid
 
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int ope = 0;
+        int clo = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                ope++;
+            } else {
+                if (ope > 0) {
+                    ope--;
+                } else {
+                    clo++;
+                }
+            }
+        }
+
+        return ope + clo;
+    }
+}
